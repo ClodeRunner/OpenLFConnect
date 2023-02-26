5e1cf5a1c562baced5247e1e99400c84c62936fa
Clode Runner <clode.runner@gmail.com>
2023-02-26 08:05:59 +0100

    Update interface.py

--- olcmodules/system/interface.py
+++ olcmodules/system/interface.py
@@ -47,7 +47,7 @@ class config(object):
     def get_device_id(self):
         try:
             return self._connection.get_device_id_i()
-        except Exception, e:
+        except Exception as e:
             self._connection.rerror(e)
 
     device_id = property(get_device_id)
@@ -57,7 +57,7 @@ class config(object):
     def get_host_id(self):
         try:
             return self._connection.get_host_id_i()
-        except Exception, e:
+        except Exception as e:
             self._connection.rerror(e)
             
     host_id = property(get_host_id)
@@ -66,8 +66,8 @@ class config(object):
     def is_connected(self):
         try:
             return self._connection.is_connected_i()
-        except Exception, e:
+        except Exception as e:
             self._connection.rerror(e)
 
 if __name__ == '__main__':
-    print 'No examples yet.'
+    print ('No examples yet.')
